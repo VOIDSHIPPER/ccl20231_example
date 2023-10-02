f3dc098a9ac9d8122311ee92de96398edf01a5ad
VOIDSHIPPER <voidshipper@gmail.com>
2023-10-02 20:44:10 +0200

    Modificacion

--- portscan.py
+++ portscan.py
@@ -15,16 +15,16 @@ def scan_ports(target_ip, start_port, end_port):
     return open_ports
 
 def main():
-    target_ip = input("Enter the target IP address: ")
-    start_port = int(input("Enter the starting port: "))
-    end_port = int(input("Enter the ending port: "))
+    target_ip = input("Enter the target IP addresseses: ")
+    start_port = int(input("Enter the starting portputo: "))
+    end_port = int(input("Enter the ending portaaaa: "))
 
     open_ports = scan_ports(target_ip, start_port, end_port)
 
     if open_ports:
-        print("Open ports on {}: {}".format(target_ip, ", ".join(map(str, open_ports))))
+        print("Open ports on ping {}: pon {}".format(target_ip, ", ".join(map(str, open_ports))))
     else:
-        print("No open ports found on {} in the specified range.".format(target_ip))
+        print("No open ports found on pingo {} in the specified range.".format(target_ip))
 
 if __name__ == "__main__":
     main()
